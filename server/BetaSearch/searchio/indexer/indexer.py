49f28382649f10ade7f6c5df614032226d707d54
Kaviarasu Sakthivadivel <kascatchme@gmail.com>
2021-11-23 13:44:27 -0500

    Initial commit for server.

---
+++ server/BetaSearch/searchio/indexer/indexer.py
@@ -0,0 +1,302 @@
+import os
+import pysolr
+import requests
+
+# https://tecadmin.net/install-apache-solr-on-ubuntu/
+
+
+CORE_NAME = "IRF21P1_demo"
+# AWS_IP = "ec2-18-218-166-215.us-east-2.compute.amazonaws.com"
+AWS_IP = "3.15.218.16"
+# AWS_IP = "localhost"
+
+
+def delete_core(core=CORE_NAME):
+    # print(os.system('sudo su - solr -c "/home/ubuntu/solr-8.11.0/bin/solr delete -c {core}"'.format(core=core)))
+    print(os.system(
+        '/Volumes/MacintoshHD/Users/kaviarasu/SoftwareTools/solr-8.9.0/bin/solr delete -c {core}'.format(core=core)))
+
+
+def create_core(core=CORE_NAME):
+    # print(os.system('sudo su - solr -c "/home/ubuntu/solr-8.11.0/bin/solr create -c {core} -n data_driven_schema_configs"'.format(core=core)))
+    print(os.system(
+        '/Volumes/MacintoshHD/Users/kaviarasu/SoftwareTools/solr-8.9.0/bin/solr create -c {core} -n data_driven_schema_configs'.format(
+            core=core)))
+
+
+class Indexer:
+    def __init__(self):
+        self.solr_url = 'http://' + AWS_IP + ':8983/solr/'
+        self.core_url = self.solr_url + CORE_NAME
+        self.connection = pysolr.Solr(self.core_url, always_commit=True, timeout=5000000)
+
+    def do_initial_setup(self):
+        delete_core()
+        create_core()
+        pass
+
+    def create_documents(self, docs):
+        print(self.connection.add(docs))
+
+    def delete_fields(self):
+        request_data = {
+            "delete-field": [
+                {
+                    "name": "lang",
+                },
+                {
+                    "name": "hashtags",
+                },
+                {
+                    "name": "mentions",
+                },
+                {
+                    "name": "tweet_urls",
+                },
+                {
+                    "name": "tweet_emoticons",
+                },
+                {
+                    "name": "tweet_date",
+                },
+                {
+                    "name": "geolocation",
+                },
+                {
+                    "name": "replied_to_tweet_id",
+                },
+                {
+                    "name": "replied_to_user_id",
+                },
+                {
+                    "name": "reply_text",
+                },
+                {
+                    "name": "text_en",
+                },
+                {
+                    "name": "text_de",
+                },
+                {
+                    "name": "text_hi",
+                }
+            ]
+        }
+
+        print(requests.post(self.core_url + "/schema", json=request_data).json())
+
+    def add_fields(self):
+        request_data = {
+            "add-field": [
+                {
+                    "name": "poi_name",
+                    "type": "string",
+                    "multiValued": False
+                },
+                {
+                    "name": "poi_id",
+                    "type": "plong",
+                    "multiValued": False
+                },
+                {
+                    "name": "verified",
+                    "type": "boolean",
+                    "multiValued": False
+                },
+                {
+                    "name": "country",
+                    "type": "string",
+                    "multiValued": False
+                },
+                {
+                    "name": "tweet_text",
+                    "type": "text_general",
+                    "multiValued": False
+                },
+                {
+                    "name": "tweet_lang",
+                    "type": "string",
+                    "multiValued": False
+                },
+                {
+                    "name": "hashtags",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "mentions",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "tweet_urls",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "tweet_emoticons",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "tweet_date",
+                    "type": "pdate",
+                    "multiValued": False
+                },
+                {
+                    "name": "geolocation",
+                    "type": "string",
+                    "multiValued": True
+                },
+                {
+                    "name": "replied_to_tweet_id",
+                    "type": "plong",
+                    "multiValued": False
+                },
+                {
+                    "name": "replied_to_user_id",
+                    "type": "plong",
+                    "multiValued": False
+                },
+                {
+                    "name": "reply_text",
+                    "type": "text_general",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_en",
+                    "type": "text_en",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_es",
+                    "type": "text_es",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_hi",
+                    "type": "text_hi",
+                    "multiValued": False
+                }
+            ]
+        }
+        print(requests.post(self.core_url + "/schema", json=request_data).json())
+
+    def replace_BM25(self, b=None, k1=None):
+        data = {
+            "replace-field-type": [
+                {
+                    'name': 'text_en',
+                    'class': 'solr.TextField',
+                    'positionIncrementGap': '100',
+                    'indexAnalyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.StopFilterFactory',
+                            'words': 'lang/stopwords_en.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.EnglishPossessiveFilterFactory'
+                        }, {
+                            'class': 'solr.KeywordMarkerFilterFactory',
+                            'protected': 'protwords.txt'
+                        }, {
+                            'class': 'solr.PorterStemFilterFactory'
+                        }]
+                    },
+                    'similarity': {
+                        'class': 'solr.BM25SimilarityFactory',
+                        'b': str(b),
+                        'k1': str(k1)
+                    },
+                    'queryAnalyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.SynonymGraphFilterFactory',
+                            'expand': 'true',
+                            'ignoreCase': 'true',
+                            'synonyms': 'synonyms.txt'
+                        }, {
+                            'class': 'solr.StopFilterFactory',
+                            'words': 'lang/stopwords_en.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.EnglishPossessiveFilterFactory'
+                        }, {
+                            'class': 'solr.KeywordMarkerFilterFactory',
+                            'protected': 'protwords.txt'
+                        }, {
+                            'class': 'solr.PorterStemFilterFactory'
+                        }]
+                    }
+                }, {
+                    'name': 'text_ru',
+                    'class': 'solr.TextField',
+                    'positionIncrementGap': '100',
+                    'analyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.StopFilterFactory',
+                            'format': 'snowball',
+                            'words': 'lang/stopwords_ru.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.SnowballPorterFilterFactory',
+                            'language': 'Russian'
+                        }]
+                    },
+                    'similarity': {
+                        'class': 'solr.BM25SimilarityFactory',
+                        'b': str(b),
+                        'k1': str(k1)
+                    },
+                }, {
+                    'name': 'text_de',
+                    'class': 'solr.TextField',
+                    'positionIncrementGap': '100',
+                    'analyzer': {
+                        'tokenizer': {
+                            'class': 'solr.StandardTokenizerFactory'
+                        },
+                        'filters': [{
+                            'class': 'solr.LowerCaseFilterFactory'
+                        }, {
+                            'class': 'solr.StopFilterFactory',
+                            'format': 'snowball',
+                            'words': 'lang/stopwords_de.txt',
+                            'ignoreCase': 'true'
+                        }, {
+                            'class': 'solr.GermanNormalizationFilterFactory'
+                        }, {
+                            'class': 'solr.GermanLightStemFilterFactory'
+                        }]
+                    },
+                    'similarity': {
+                        'class': 'solr.BM25SimilarityFactory',
+                        'b': str(b),
+                        'k1': str(k1)
+                    },
+                }
+            ]
+        }
+
+        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
+
+# if __name__ == "__main__":
+#     i = Indexer()
+#     i.do_initial_setup()
+#     i.delete_fields()
+#     i.add_fields()
---
+++ server/BetaSearch/searchio/indexer/utils.py
@@ -0,0 +1,42 @@
+import json
+
+import requests
+
+from server.BetaSearch.searchio.indexer.indexer import Indexer
+
+
+def download_data(core, aws_url, total_docs):
+    count = 1000
+    pages = int(total_docs / count)
+    rows = count
+    for i in range(pages):
+        print(i)
+        start = i * count
+        response = requests.get('http://' + aws_url + ':8983/solr/' + core + '/query?q=*&start='
+                                + str(start) + '&rows=' + str(rows))
+        json_response = response.json()
+        # print(json_response)
+        docs = json_response['response']['docs']
+        processed_docs = []
+        for doc in docs:
+            del doc['_version_']
+            processed_docs.append(doc)
+        index_data(processed_docs)
+        # with open('data/' + str(i) + '.json', 'w') as outfile:
+        #     json.dump(docs, outfile)
+
+
+def index_data(docs):
+    # indexer.create_documents(processed_tweets)
+    i.create_documents(docs)
+
+
+if __name__ == '__main__':
+    i = Indexer()
+    # i.do_initial_setup()
+    # i.delete_fields()
+    # i.add_fields()
+    download_data(core='IRF21P1_demo', aws_url='18.191.161.74', total_docs=179000)  # Kavi
+    download_data(core='IRF21P1', aws_url='18.217.75.107', total_docs=179000)  # Pak
+    download_data(core='IRF21P1', aws_url='18.223.120.151', total_docs=73000)  # Aashiq
+    download_data(core='IRF21P1', aws_url='3.136.97.247', total_docs=75000)  # Daya
