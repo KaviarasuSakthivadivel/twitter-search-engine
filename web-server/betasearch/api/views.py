eec66cd8f2e5a4632877d2d500908ee95f49786b
Deepak0797 <hariharishdeepak@gmail.com>
2021-11-24 23:18:56 -0500

    changed search method

--- web-server/betasearch/api/views.py
+++ web-server/betasearch/api/views.py
@@ -2,21 +2,47 @@ import requests
 from django.conf import settings
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+import urllib.request
+from googletrans import Translator
+import json
 
+
+translator = Translator()
 
 @api_view(['GET', 'POST'])
 def search(request):
     if request.method == 'GET':
         query = request.GET['query']
         start = request.GET['start']
-        offset = 10
+        row=request.GET['row']
+        pageNo=int(request.GET['pageNo'])
+        pageSize=int(request.GET['pageSize'])
         if query is None:
             query = '*'
         if start is None:
             start = 0
-        response = requests.get('http://' + settings.AWS_URL + ':8983/solr/' + settings.CORE + '/query?q='
-                                + query + '&start=' + str(start) + '&rows=' + str(offset))
-        json_response = response.json()
-        print(json_response)
-        return JsonResponse(json_response, safe=False)
+        else:
+            start=(pageNo-1)*pageSize
+        if row is None:
+            row=0
+        else:
+            row=pageSize
+        #translate the given query in all three languages(en,hi,es) to support search results in all three languages
+        translator_en=translator.translate(query, dest='en')
+        query_en=translator_en.text
+        translator_es=translator.translate(query, dest='es')
+        query_es=translator_es.text
+        translator_hi=translator.translate(query, dest='hi')
+        query_hi=translator_hi.text
+        
+        #query generation for text_en ,text_hi,text_es fields 
+        query="text_en:"+"("+query+")"+" OR "+"text_es:"+"("+query+")"+" OR "+"text_hi:"+"("+query+")"+"text_en:"+"("+query_en+")"+" OR "+"text_es:"+"("+query_es+")"+" OR "+"text_hi:"+"("+query_hi+")"
+        q=urllib.parse.quote(query, encoding="UTF-8")
 
+        #hit Solr and get the docs with paination for each start and row combination 
+        response = requests.get('http://' + settings.AWS_URL + ':8983/solr/' + settings.CORE + '/query?q='
+                                + q + '&start=' + str(start) + '&rows=' + str(row))
+        json_response = response.json()
+        #results = json_response['response']
+        #print(json_response)
+        return JsonResponse(json_response)
