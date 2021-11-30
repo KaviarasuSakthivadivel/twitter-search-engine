dfea84ad1c3929578debd315e987a9f0b4483b8a
Kaviarasu Sakthivadivel <kascatchme@gmail.com>
2021-11-29 22:07:37 -0500

    Not null checks for the fields.

--- web-server/betasearch/api/views.py
+++ web-server/betasearch/api/views.py
@@ -4,8 +4,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 import urllib.request
 from googletrans import Translator
-import datetime 
-
+import datetime
 
 translator = Translator()
 
@@ -16,24 +15,53 @@ def search(request):
         query = request.GET['query']
         pageNo = int(request.GET['pageNo'])
         pageSize = int(request.GET['pageSize'])
-        poiName = str(request.GET['poiName'])
-        tweetLang = str(request.GET['tweetLang'])
-        country = str(request.GET['country'])
-        timestamp = float(request.GET['timestamp'])
-        mentions = str(request.GET['mentions'])
-        showOnlyReplies = bool(request.GET['showOnlyReplies'])
-        showOnlyPoi = bool(request.GET['showOnlyPoi'])
-        showTweetsWithLinks = bool(request.GET['showTweetsWithLinks'])
-        replyCount = int(request.GET['replyCount'])
-        hashtags = str(request.GET['hashtags'])
-        fQuery = None
+        poiName = None
+        tweetLang = None
+        country = None
+        timestamp = None
+        mentions = None
+        show_only_replies = False
+        show_only_poi = False
+        showTweetsWithLinks = False
+        replyCount = 0
+        hashtags = None
+        if request.GET.get('poiName', None) is not None:
+            poiName = str(request.GET['poiName'])
+
+        if request.GET.get('tweetLang', None) is not None:
+            tweetLang = str(request.GET['tweetLang'])
+
+        if request.GET.get('country', None) is not None:
+            country = str(request.GET['country'])
+
+        if request.GET.get('timestamp', None) is not None:
+            timestamp = float(request.GET['timestamp'])
+
+        if request.GET.get('mentions', None) is not None:
+            mentions = str(request.GET['mentions'])
+
+        if request.GET.get('showOnlyReplies', False) is not False:
+            show_only_replies = bool(request.GET['showOnlyReplies'])
+
+        if request.GET.get('showOnlyPoi', False) is not False:
+            show_only_poi = bool(request.GET['showOnlyPoi'])
+
+        if request.GET.get('showTweetsWithLinks', False) is not False:
+            showTweetsWithLinks = bool(request.GET['showTweetsWithLinks'])
+
+        if request.GET.get('replyCount', 0) is not 0:
+            replyCount = int(request.GET['replyCount'])
+
+        if request.GET.get('hashtags', None) is not None:
+            hashtags = str(request.GET['hashtags'])
+        f_query = None
         if query is None:
             query = '*'
-        if fQuery is None:
-            fQuery = '*'
-        start = (pageNo-1)*pageSize
+        if f_query is None:
+            f_query = '*'
+        start = (pageNo - 1) * pageSize
         row = pageSize
-        
+
         # translate the given query in all three languages(en,hi,es) to support search results in all three languages
         translator_en = translator.translate(query, dest='en')
         query_en = translator_en.text
@@ -43,68 +71,70 @@ def search(request):
         query_hi = translator_hi.text
 
         # query generation for text_en ,text_hi,text_es fields
-        query = "text_en:"+"("+query+")^10"+" OR "+"text_es:"+"("+query+")^10"+" OR "+"text_hi:"+"("+query+")^10" + \
-            "text_en:"+"("+query_en+")"+" OR "+"text_es:" + \
-            "("+query_es+")"+" OR "+"text_hi:"+"("+query_hi+")"
+        query = "text_en:" + "(" + query + ")^10" + " OR " + "text_es:" + "(" + query + ")^10" + " OR " + "text_hi:" + "(" + query + ")^10" + \
+                "text_en:" + "(" + query_en + ")" + " OR " + "text_es:" + \
+                "(" + query_es + ")" + " OR " + "text_hi:" + "(" + query_hi + ")"
         q = urllib.parse.quote(query, encoding="UTF-8")
-        
+
         # POI Name filter
         if poiName is not None:
-            fQuery = fQuery + " AND "+"poi_name:"+"("+poiName+")"
+            f_query = f_query + " AND " + "poi_name:" + "(" + poiName + ")"
 
         # Language filter
         if tweetLang is not None:
-            fQuery = fQuery + " AND "+"tweet_lang:"+"("+tweetLang+")"
+            f_query = f_query + " AND " + "tweet_lang:" + "(" + tweetLang + ")"
 
         # Country filter
         if country is not None:
-            fQuery = fQuery + " AND "+"country:"+"("+country+")"
+            f_query = f_query + " AND " + "country:" + "(" + country + ")"
 
         # Created Time
         if timestamp is not None:
             timestamp = datetime.date.fromtimestamp(timestamp)
             tweetDate = timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
-            fQuery = fQuery + " AND "+"tweet_date:"+"["+tweetDate+" TO NOW"+"]"
+            f_query = f_query + " AND " + "tweet_date:" + "[" + tweetDate + " TO NOW" + "]"
 
         # mentions filter
         if mentions is not None:
-            fQuery = fQuery + " AND "+"mentions:"+"("+mentions+")"
+            f_query = f_query + " AND " + "mentions:" + "(" + mentions + ")"
 
         # show only replies filter
-        if showOnlyReplies is True:
-            fQuery = fQuery + " AND "+"reply_text:"+"*"
+        if show_only_replies is True:
+            f_query = f_query + " AND " + "reply_text:" + "*"
 
         # show only poi tweets filter
-        if showOnlyPoi is True:
-            fQuery = fQuery + " AND "+"poi_name:"+"*"
+        if show_only_poi is True:
+            f_query = f_query + " AND " + "poi_name:" + "*"
 
         # show only tweets with replies filter
         if showTweetsWithLinks is True:
-            fQuery = fQuery + " AND "+"tweet_text:"+"(http)"
+            f_query = f_query + " AND " + "tweet_text:" + "(http)"
 
         # minimum replies filter
-        if replyCount is not None:
-            fQuery = fQuery + " AND "+"reply_count:"+"["+replyCount+"TO *"+"]"
+        if replyCount is not 0:
+            f_query = f_query + " AND " + "reply_count:" + "[" + str(replyCount) + "TO *" + "]"
 
         # hashtags filter
         if hashtags is not None:
-            hashtags.replace("#","")
-            fQuery = fQuery + " AND "+"hashtags:"+"("+hashtags+")"
+            hashtags.replace("#", "")
+            f_query = f_query + " AND " + "hashtags:" + "(" + hashtags + ")"
 
         # hit Solr and get the docs with paination for each start and row combination
-        if fQuery is not None:
+        if f_query is not None:
             # appending fq
-            fq = urllib.parse.quote(fQuery, encoding="UTF-8")
-            finalQuery = 'http://' + settings.AWS_URL + ':8983/solr/' + settings.CORE + '/query?q=' + \
-                        q + '&fq=' + fq + '&start=' + str(start) + '&rows=' + str(row) + \
-                        '&hl=true&hl.requireFieldMatch=false&hl.usePhraseHighLighter=false&hl.highlightMultiTerm=false&hl.fl=tweet_text'
-        else:          
-            finalQuery = 'http://' + settings.AWS_URL + ':8983/solr/' + settings.CORE + '/query?q=' + \
-                        q + '&start=' + str(start) + '&rows=' + str(row) + \
-                        '&hl=true&hl.requireFieldMatch=false&hl.usePhraseHighLighter=false&hl.highlightMultiTerm=false&hl.fl=tweet_text'
+            fq = urllib.parse.quote(f_query, encoding="UTF-8")
+            final_query = 'http://' + settings.AWS_URL + ':8983/solr/' + settings.CORE + '/query?q=' + \
+                          q + '&fq=' + fq + '&start=' + str(start) + '&rows=' + str(row) + \
+                          '&hl=true&hl.requireFieldMatch=false&hl.usePhraseHighLighter=false&hl.highlightMultiTerm' \
+                          '=false&hl.fl=tweet_text '
+        else:
+            final_query = 'http://' + settings.AWS_URL + ':8983/solr/' + settings.CORE + '/query?q=' + \
+                          q + '&start=' + str(start) + '&rows=' + str(row) + \
+                          '&hl=true&hl.requireFieldMatch=false&hl.usePhraseHighLighter=false&hl.highlightMultiTerm' \
+                          '=false&hl.fl=tweet_text '
 
-        response = requests.get(finalQuery)
+        response = requests.get(final_query)
         json_response = response.json()
-        #results = json_response['response']
+        # results = json_response['response']
         # print(json_response)
         return JsonResponse(json_response)
